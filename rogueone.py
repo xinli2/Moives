b240414f5b6664077f734065c28043d13913d972
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2021-10-29 13:32:01 -0700

    Create rogueone.py

---
+++ rogueone.py
@@ -0,0 +1,18 @@
+###
+### Author: Xin Li
+### Description: Rogue One: A Star Wars Story: scene's movie scripts
+
+print('''----- ROGUE ONE -----
+
+
+BODHI: That's him. That's him, Galen. In the dark suit.
+
+CASSIAN: Get back down there, and find us a ride out of here.
+
+BODHI: What will you do?
+
+CASSIAN: You heard me.
+
+BODHI: You said we came up here just to have a look.
+
+CASSIAN: I'm here. I'm looking. Go. Hurry!''')
