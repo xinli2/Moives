4894ca6c72e9e8b532e7f4bf56698bd16d3b9489
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2021-10-29 13:30:42 -0700

    Create gladiator.py

---
+++ gladiator.py
@@ -0,0 +1,23 @@
+###
+### Author: Xin Li
+### Description: GLADTATOR:POST-FIGHT SCENE's movie scripts
+
+print('''----- GLADIATOR -----
+
+
+EMPEROR: Rise. Rise. Your fame is well deserved, Spaniard. I don't think there's
+         ever been a gladiator to match you. As for this young man, he insists
+         you are Hector reborn. Or was it Hercules? Why doesn't the hero reveal
+         himself and tell us all your real name? You do have a name.
+
+MAXIMUS: My name is Gladiator.
+
+EMPEROR: How dare you show your back to me? Slave! You will remove your helmet
+         and tell me your name.
+
+MAXIMUS: My name is Maximus Decimus Meridius, commander of the armies of the
+         north, general of the Felix Legions, loyal servant to the true emperor,
+         Marcus Aurelius, father to a murdered son, husband to a murdered wife,
+         and I will have my vengeance, in this life or the next.
+
+SOLDIER: Arms!''')
