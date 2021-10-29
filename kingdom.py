01f0f916519d3190493cae43f5665a19b28a8b70
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2021-10-29 13:31:17 -0700

    Create kingdom.py

---
+++ kingdom.py
@@ -0,0 +1,22 @@
+###
+### Author: Xin Li
+### Description: KINGDOM OF HEAVEN's movie scripts
+
+print('''----- KINGDOM OF HEAVEN -----
+
+
+KING: Do you play?
+
+BALIAN: No.
+
+KING: The whole world is in chess. Any move can be the death of you. Do anything
+      except remain where you started and you can't be sure of your end. Were
+      you sure of your end once?
+
+BALIAN: I was.
+
+KING: What was it?
+
+BALIAN: To be buried 100 yards from where I was born.
+
+KING: And now?''')
